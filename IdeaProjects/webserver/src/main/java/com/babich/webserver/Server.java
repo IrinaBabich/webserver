f1428e8adaaa76cbe8284706ebd81d0957fe0cf4
Irina Babich <iringjob@gmail.com>
2018-06-17 00:48:31 +0300

    first commit

--- IdeaProjects/webserver/src/main/java/com/babich/webserver/Server.java
+++ IdeaProjects/webserver/src/main/java/com/babich/webserver/Server.java
@@ -5,17 +5,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    String webappPath;
+
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(3001);
+        try (ServerSocket server = new ServerSocket(3000);
              Socket socket = server.accept();
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            String message = reader.readLine();
-            System.out.println("message from client: " + message);
+            while (true) {
+                String line = reader.readLine();
+                // GET /index.html HTTP/1.1
+                String uri = split(" ")[1]; // положили index.html
+                new FileReader(webappPath + uri);
 
-            writer.write("echo: " + message);
-            writer.newLine();
+
+
+                if (line.isEmpty()) {
+                    break;
+                }
+                System.out.println("message from client: " + line);
+            }
+
+            writer.write("HTTP/1.1 200 OK\n");
+            writer.write("\n");
+            writer.write("Hello world");
             writer.flush();
         }
     }
---
+++ IdeaProjects/webserver/src/test/java/ServerITest.java
@@ -0,0 +1,25 @@
+import com.babich.webserver.Server;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServerITest {
+    @Test
+    public void testServer() {
+        Server server = new Server();
+        server.setPort(3000);
+        server.setWebAppPath("src/main/resources/webapp");
+        server.start();
+    }
+}
+
+
+// localhost: 3000/index.html
+//           localhost:3000/style/styles.css
+
+//src/main/resources/webapp/index.html -> send as response with status 200 OK
+//src/main/resources/webapp/style/styles.css -> send as response with status 200 OK
+
+//localhost:3000/unknownResource -> 404 not found
+
+// day stranitsy -> Bad request
