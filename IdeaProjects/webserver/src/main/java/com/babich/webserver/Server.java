eacf7d26b44fe05a0d5aad33a2f5627bc9acb2ef
Irina Babich <iringjob@gmail.com>
2018-06-18 19:15:28 +0300

    second commit

--- IdeaProjects/webserver/src/main/java/com/babich/webserver/Server.java
+++ IdeaProjects/webserver/src/main/java/com/babich/webserver/Server.java
@@ -5,32 +5,54 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    String webappPath;
+    private String webAppPath;
+    private int port;
+    private String unknownResource = "HTTP/1.1 404 not found\n";
+    private String returnPage = "Bad request";
+    private String OK = "HTTP/1.1 200 OK\n";
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(3000);
+    public void start() throws IOException {
+        try (ServerSocket server = new ServerSocket(port);
+             // accept говорит серверу об ожидании клиента, ждем подключения
              Socket socket = server.accept();
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
             while (true) {
                 String line = reader.readLine();
-                // GET /index.html HTTP/1.1
-                String uri = split(" ")[1]; // положили index.html
-                new FileReader(webappPath + uri);
-
-
-
+                // пустая строка, если request закончился
                 if (line.isEmpty()) {
                     break;
                 }
                 System.out.println("message from client: " + line);
+
+                // GET /index.html HTTP/1.1
+                String uri = line.split(" ")[1]; // положили index.html
+
+                String resourcePath = webAppPath+uri;
+
+                // если файла не существует, то на веб-страницу выдает ошибку
+                File file = new File(resourcePath);
+                if (!file.exists()) {
+                    writer.write(unknownResource);
+                }
+
+//                try (BufferedReader fileReader = new BufferedReader(new FileReader(resourcePath));
+
             }
 
             writer.write("HTTP/1.1 200 OK\n");
             writer.write("\n");
-            writer.write("Hello world");
+//            writer.write(content);
             writer.flush();
         }
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setWebAppPath(String webAppPath) {
+        this.webAppPath = webAppPath;
     }
 }
--- IdeaProjects/webserver/src/test/java/ServerITest.java
+++ IdeaProjects/webserver/src/test/java/ServerITest.java
@@ -1,11 +1,12 @@
 import com.babich.webserver.Server;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+
 
 public class ServerITest {
     @Test
-    public void testServer() {
+    public void testServer() throws IOException {
         Server server = new Server();
         server.setPort(3000);
         server.setWebAppPath("src/main/resources/webapp");
@@ -15,11 +16,10 @@ public class ServerITest {
 
 
 // localhost: 3000/index.html
-//           localhost:3000/style/styles.css
+// localhost:3000/style/styles.css
 
 //src/main/resources/webapp/index.html -> send as response with status 200 OK
 //src/main/resources/webapp/style/styles.css -> send as response with status 200 OK
 
 //localhost:3000/unknownResource -> 404 not found
-
 // day stranitsy -> Bad request
